0a541b98ef692d4dabad2c48a1d5a9413ed2a1a0
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2014-12-14 18:19:20 -0800

    Add subtitle and corresponding defined tag

--- directory-require.rkt
+++ directory-require.rkt
@@ -1,4 +1,5 @@
 #lang racket
 
 (define (page-title . title) `(h1 ,@title))
-(provide page-title)
+(define (subtitle . text) `(p ((class "subtitle")) ,@text))
+(provide page-title subtitle)
--- introduction.html.pm
+++ introduction.html.pm
@@ -1,5 +1,6 @@
 #lang pollen
 
-◊header{
+◊header['id:"page-header"]{
 	◊page-title{Contrapunctus}
+	◊subtitle{An Introduction to Modal Counterpoint}
 }
