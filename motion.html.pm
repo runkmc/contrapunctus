8ec5a68c60bba684442420bc9fd2abcc3e691f30
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2014-12-18 10:00:53 -0800

    Begin to write about motion

--- motion.html.pm
+++ motion.html.pm
@@ -1,1 +1,10 @@
 #lang pollen
+
+◊header['id:"page-header"]{
+	◊page-title{Motion}
+}
+◊section['class:"main-body"]{
+	◊section-title{Motion in a Single Part}
+	◊p{Take a look at the following musical example. These }
+}
+
