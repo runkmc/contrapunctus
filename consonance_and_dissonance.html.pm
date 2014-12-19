234b7a616cf04a7f61f3f5dea0033966b661ea50
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2014-12-18 23:28:57 -0800

    Write chapter on consonance and dissonance

--- consonance_and_dissonance.html.pm
+++ consonance_and_dissonance.html.pm
@@ -1,1 +1,46 @@
 #lang pollen
+
+◊header['id:"page-header"]{
+	◊page-title{Consonance and Dissonance}
+}
+◊main-section{
+	◊p{Now that we've learned how to identify the different intervals, it's
+	time to learn how to classify them. Not all intervals are created equal,
+	and the rules of counterpoint depend heavily on these different
+	classifications. We will discuss which intervals are consonant, which
+	intervals are dissonant, and the different types of consonance. In the next
+	chapter, you will see why all of this matters.}
+	◊sidebar{If you've studied music theory, you'll quickly realize I'm leaving
+	out quite a bit when it comes to the different qualities of intervals. This
+	is intentional as these differences – with one exception I'll address
+	shortly – don't really matter
+	in modal counterpoint.}
+	}
+◊main-section{
+	◊section-title{Consonant Intervals}
+	◊p{The consonant intervals are the ◊em{third,} the ◊em{fifth,} the
+	◊em{sixth,} and the ◊em{octave. Tenths, twelfths,} and ◊em{thirteenths} are
+	also consonant, since these intervals are just an octave plus a third,
+	fifth, and sixth, respectively.}
+	◊p{These consonances break down further into ◊em{perfect consonances} and
+	◊em{imperfect consonances}. The third and the sixth (as well as the tenth
+		and thirteenth) are imperfect. The fifth and the octave (as well as the
+	twelfth) are perfect. This distinction is important, as we will find out in
+	the next chapter.}}
+◊main-section{
+	◊section-title{Dissonant Intervals}
+	◊p{◊em{Seconds, fourths,} and ◊em{sevenths} are all dissonant.}
+	◊p{That's it!}
+	◊p{Well, that's not entirely it. There is one more dissonance that you will
+	have to watch out for. Here are a whole bunch of fifths:}
+	◊img['src:"lilypond/dissonance1.svg"]
+	◊p{Do you see that interval with a big X underneath it? That fifth is
+	actually dissonant! Here's why: every other fifth in this example is
+	exactly seven half steps apart, but this particular fifth, from B to F, is
+	only six half steps apart. Instead of a ◊em{perfect} fifth, this
+	forms a ◊em{diminished} fifth. This is the only interval in this system
+	that can be consonant or dissonant depending on its location, and these are
+	the only two pitches (B and F) that form it, so make sure
+	you watch out for it.}
+	◊p{◊em{(Here are the answers to the exercise from the last chapter: 1, 3, 7, 4,
+	8, 10, 13, 8, 7, 8.)}}}
