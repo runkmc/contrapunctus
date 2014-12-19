337dff99b4f7f0824135a32e0577774f8f7a7a54
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2014-12-18 18:34:37 -0800

    Continue writing chapter on intervals

---
+++ intervals.html.pm
@@ -0,0 +1,27 @@
+#lang pollen
+
+◊header['id:"page-header"]{
+	◊page-title{Intervals}
+}
+◊section['class:"main-body"]{
+	◊p{An ◊em{interval} is the distance between two pitches. Being able to figure
+	out the interval between two pitches is absolutely necessary when it comes
+	to writing modal counterpoint. The good news is that if you can count to
+	twelve or thirteen, you can master this skill pretty quickly.}
+	◊p{Intervals are determined by the number of lines and spaces between two
+	given notes. Observe:}
+	◊img['src:"lilypond/intervals1.svg"]
+	◊p{Notice that instead of starting at zero, the interval between two
+	identical notes is one. After that, they continue in a straightforward
+	manner.}
+}
+◊main-section{
+	◊section-title{Interval Names}
+	◊p{Intervals are generally referred to by their ordinal names. That is, two
+	pitches that are separated by an interval of two form a ◊em{second},
+	pitches that form an interval of three form a ◊em{third}, and so on, up to
+	an ◊em{eleventh} and a ◊em{twelfth} in the above example. There are two
+	exceptions to this rule. Identical pitches (that is, pitches with an interval 
+	of 1) are usually referred to as a ◊em{unison}, and pitches separated by an
+	interval of eight are referred to as an ◊em{octave}.}
+	}
