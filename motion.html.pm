739f18c2296a5de4679bfcb6a95ea6f6b780d844
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2014-12-21 00:37:08 -0800

    Continue writing motion chapter

--- motion.html.pm
+++ motion.html.pm
@@ -19,3 +19,26 @@
 	an interval of a fourth or more. This one is a leap of a fifth.}
 	◊img['src:"lilypond/motion4.svg"]
 }
+◊main-section{
+	◊section-title{Motion Between Two Parts}
+	◊p{Movement within a single part is easy enough to describe, but we will
+	also need a way to describe the different types of motion that can occur
+	between two parts. In other words, motion in a single part could also be
+	called ◊em{horizontal motion.} Now, we'll be discussing ◊em{vertical
+	motion.}}
+	◊p{There are essentially four different types of motion that can occur
+	between two parts moving together. These four types are ◊em{parallel,
+	similar, contrary, } and ◊em{oblique.}}
+	◊p{◊em{Parallel} motion occurs when two voices move the same interval in
+	the same direction. Here are some examples of parallel motion:}
+	◊img['src:"lilypond/motion5.svg"]
+	◊p{◊em{Similar} motion occurs when two voices move in the same direction,
+	but at different intervals. For example, one voice might ascend by a step
+	while the second voice ascends by a skip. Examples are below:}
+	◊img['src:"lilypond/motion6.svg"]
+	◊p{◊em{Contrary} motion describes two voices moving in opposite directions.
+	It doesn't matter if the voices are moving by step, skip, or leap. If they
+	are moving in opposite directions, their motion is contrary like these
+	examples:}
+	◊img['src:"lilypond/motion7.svg"]
+	}
