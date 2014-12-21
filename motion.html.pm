1300595e9768980366beb153be8a4116fb05f867
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2014-12-20 23:57:10 -0800

    Continue writing motion chapter

--- motion.html.pm
+++ motion.html.pm
@@ -5,6 +5,17 @@
 }
 ◊section['class:"main-body"]{
 	◊section-title{Motion in a Single Part}
-	◊p{Take a look at the following musical example. These }
+	◊p{Take a look at the following example:}
+	◊img['src:"lilypond/motion1.svg"]
+	◊p{This short example features the three types of melodic motion 
+	that we'll be discussing: ◊em{steps, skips,} and ◊em{leaps.}}
+	◊p{A ◊em{step} is when a melody moves by the interval of a second. Here is
+	the same example as before, but now with the steps all marked:}
+	◊img['src:"lilypond/motion2.svg"]
+	◊p{Skips are melodic movement by a third. This happens twice in the
+	example; I've marked them below.}
+	◊img['src:"lilypond/motion3.svg"]
+	◊p{There is exactly one leap in the example below. A leap is movement by
+	an interval of a fourth or more. This one is a leap of a fifth.}
+	◊img['src:"lilypond/motion4.svg"]
 }
-
