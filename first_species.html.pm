5fe6b05d420303bdaf216b82ae1c57b8b70cc776
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2014-12-24 01:01:44 -0800

    Begin first species chapter

---
+++ first_species.html.pm
@@ -0,0 +1,11 @@
+#lang pollen
+
+◊header['id:"page-header"]{
+	◊page-title{First Species}
+	}
+◊main-section{
+	◊p{If your vision isn't too blurred from all that trembling with excitement
+	you're undoubtedly experiencing, it's time to prepare for your very first
+	counterpoint exercise.}
+	◊p{Here is how this is going to go down. I'm going to give you a melody,
+		and you're going to write a second melody above it.}}
