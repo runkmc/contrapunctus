17491cbf99389c5a6555dcc9882d70872a553931
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2015-01-02 00:43:45 -0800

    Continue writing First Species chapter

--- first_species.html.pm
+++ first_species.html.pm
@@ -8,4 +8,17 @@
 	you're undoubtedly experiencing, it's time to prepare for your very first
 	counterpoint exercise.}
 	◊p{Here is how this is going to go down. I'm going to give you a melody,
-		and you're going to write a second melody above it.}}
+	and you're going to write a second melody above it. Before we begin, we
+	will talk a little about the melody I'm giving you, as well as the rules for
+	writing your melody.}}
+◊main-section{◊subtitle{The Cantus Firmus}
+	◊p{Every melody I give you from here on out in the various exercises will
+	be referred to as a ◊term{Cantus Firmus.} This roughly translated to
+	◊em{fixed melody,} and it means that you're not allowed to change it no
+	matter how much you may want to so ha ha ha. The only thing you're allowed
+	to create or change in these exercises is your own melody. A cantus firmus
+	will look something like this:}
+	◊img['src:"lilypond/cantus-firmus-example.svg"]
+	}
+
+
