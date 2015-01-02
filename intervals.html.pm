175cf1d35adeb86ec6055f820fa29f152fae2434
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2015-01-02 00:22:21 -0800

    Switch from em to term tags

--- intervals.html.pm
+++ intervals.html.pm
@@ -5,14 +5,14 @@
 }
 ◊main-section{
 	◊p{If you've ever sung in a choir or played an instrument, you might be
-	familiar with the notes of the ◊em{staff} with a ◊em{treble clef:}}
+	familiar with the notes of the ◊term{staff} with a ◊term{treble clef:}}
 	◊img['src:"lilypond/pitches1.svg"]
-	◊p{Only slight less familiar is the ◊em{bass clef.} Note that the C at the
+	◊p{Only slight less familiar is the ◊term{bass clef.} Note that the C at the
 	very top of the bass clef example is the exact same pitch as the lowest C on
 	the above treble clef example (sometimes called "middle C")}
 	◊img['src:"lilypond/pitches2.svg"]
 	◊p{The last clef we'll be using (and one I'll be introducing gradually) is
-	the ◊em{alto clef.} This clef places middle C on the third line, in the
+	the ◊term{alto clef.} This clef places middle C on the third line, in the
 	very center of the staff.}
 	◊img['src:"lilypond/pitches3.svg"]
 	◊p{Knowing how these clefs relate to each other is going to be very
@@ -21,7 +21,7 @@
 }
 ◊main-section{
 	◊section-title{Intervals}
-	◊p{An ◊em{interval} is the distance between two pitches. Being able to figure
+	◊p{An ◊term{interval} is the distance between two pitches. Being able to figure
 	out the interval between two pitches is absolutely necessary when it comes
 	to writing modal counterpoint. The good news is that if you can count to
 	twelve or thirteen, you can master this skill pretty quickly.}
@@ -36,12 +36,12 @@
 ◊main-section{
 	◊section-title{Interval Names}
 	◊p{Intervals are generally referred to by their ordinal names. That is, two
-	pitches that are separated by an interval of two form a ◊em{second,}
-	pitches that form an interval of three are called a ◊em{third,} and so on, up to
-	an ◊em{eleventh} and a ◊em{twelfth} in the above example. There are two
+	pitches that are separated by an interval of two form a ◊term{second,}
+	pitches that form an interval of three are called a ◊term{third,} and so on, up to
+	an ◊term{eleventh} and a ◊term{twelfth} in the above example. There are two
 	exceptions to this rule. Identical pitches (that is, pitches with an interval 
-	of one) are usually referred to as a ◊em{unison}, and pitches separated by an
-	interval of eight are referred to as an ◊em{octave.}}
+	of one) are usually referred to as a ◊term{unison}, and pitches separated by an
+	interval of eight are referred to as an ◊term{octave.}}
 	}
 ◊main-section{
 	◊section-title{Exercises}
