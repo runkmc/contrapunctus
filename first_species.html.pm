20492f921220f6283534027e5d00094c8dab204a
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2015-01-03 11:45:13 -0800

    Continue writing First Species chapter

--- first_species.html.pm
+++ first_species.html.pm
@@ -11,14 +11,47 @@
 	and you're going to write a second melody above it. Before we begin, we
 	will talk a little about the melody I'm giving you, as well as the rules for
 	writing your melody.}}
-◊main-section{◊subtitle{The Cantus Firmus}
+◊main-section{◊section-title{The Cantus Firmus}
 	◊p{Every melody I give you from here on out in the various exercises will
-	be referred to as a ◊term{Cantus Firmus.} This roughly translated to
+	be referred to as a ◊term{cantus firmus.} This roughly translates to
 	◊em{fixed melody,} and it means that you're not allowed to change it no
-	matter how much you may want to so ha ha ha. The only thing you're allowed
+	matter how much you may want to so ha ha ha deal with it. The only thing you're allowed
 	to create or change in these exercises is your own melody. A cantus firmus
 	will look something like this:}
 	◊img['src:"lilypond/cantus-firmus-example.svg"]
-	}
-
+	◊p{There are a couple of characteristics found here that almost every cantus firmus
+	will have. They will all be made up of a series of whole notes, and they
+	will always start and end on the same note. Their length can vary, but most
+	of them will be between eight and ten notes long. I'll also always
+	give them to you in alto clef. This is mostly because learning alto clef is
+	good for you, and only partially because I hate you and want you to feel
+	pain.
+	}}
+◊main-section{◊section-title{First Species Rules}
+	◊p{Now before your can start your exercise, you need to know the rules for
+	writing first species counterpoint. What, did you think there were no
+	rules? Of course there are rules. Without rules, we would descend into
+	madness like that kid in ◊em{Lord of the Flies.} You know. That book with
+	the flies. And the kid. Who was the lord of them.}
+	◊p{There is some good news and bad news about the rules. The bad news is
+	that there are a lot of them. The good news is that most of them hold for
+	all the exercises we will be doing for the rest of the book, sometimes with
+	slight variations. To help keep track of them, I'm going to break them down
+	into a few different groupings. First, here is the most important rule in
+	first species counterpoint:}
+		◊ul{
+			◊li{For every note in the cantus firmus, you will have one
+			corresponding note in your own melody.}
+			}
+	◊p{Easy enough, right? Here are some rules having to do with intervals:}
+		◊ul{
+			◊li{Every interval between each note in the cantus firmus and your
+			melody has to be consonant.}
+			◊li{The first and last intervals of the entire exercise need to be
+			perfect consonances.}
+			◊li{Your melody may never leap from F to B or the inverse, from
+			B to F.}}
+	◊p{Here are some rules regarding motion:
+		◊ul{}}
+			}
 
