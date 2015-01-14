b51a328a978b73ed02858939f3eccef224c77031
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2015-01-14 11:42:51 -0800

    Begin new chapter

---
+++ first_species_exercises.html.pm
@@ -0,0 +1,17 @@
+#lang pollen
+
+◊header['id:"page-header"]{
+	◊page-title{First Species Exercises}
+	}
+◊main-section{◊p{In an ideal world, I would be right there to help you with your
+	exercises. In fact, if you desire some help, feel free to contact me.
+	I would seriously be more than happy to give feedback. If you don't believe
+	me, keep in mind that I wrote an online booklet about this very subject for
+	kicks. That's how unbalanced I am. I like this stuff.}
+	◊p{In the meantime, I've done the exercises myself, twice. First we will
+	look at answers with some errors. I'll ask you to try to find as many
+	mistakes as possible, then I'll point them out myself. After that, I'll
+	offer two examples that are hopefully much better. Here are my two
+	mistake-ridden tries:}
+	◊img['src:"lilypond/first_example_bad1.svg" 'class:"double"]
+	}
