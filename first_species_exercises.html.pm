1d955a4e1814b32b9881fd782a2ef2d0b0d0d3ba
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2015-01-22 20:54:02 -0800

    Explain first bad example in First Species

--- first_species_exercises.html.pm
+++ first_species_exercises.html.pm
@@ -9,9 +9,22 @@
 	me, keep in mind that I wrote an online booklet about this very subject for
 	kicks. That's how unbalanced I am. I like this stuff.}
 	◊p{In the meantime, I've done the exercises myself, twice. First we will
-	look at answers with some errors. I'll ask you to try to find as many
+	look at an answer with some errors. I'll ask you to try to find as many
 	mistakes as possible, then I'll point them out myself. After that, I'll
-	offer two examples that are hopefully much better. Here are my two
-	mistake-ridden tries:}
+	offer an example that is hopefully much better. Then, we'll do it all over
+	again with the second exercise. Here we go:}
 	◊img['src:"lilypond/first_example_bad1.svg" 'class:"double"]
+	}
+◊main-section{
+	◊section-title{Exercise 1}
+	◊p{There are only two mistakes in this example that break the rules I gave
+	you in the last chapter. (There are some other problems with it, but that's
+	for the chapter titled "Things I Didn't Talk About But Maybe Should Have.")
+	The first mistake occurs in the movement between the fifth and sixth notes.
+	I've written two intervals of a fifth, back to back. That's a no-no.
+	Anytime you have consecutive fifths (or octaves), that's an error. The
+	second mistake is in the ninth interval. I have an intervals of a fourth
+	there, but that's a dissonance. Only consonances are allowed here.}
+	◊p{Yikes. That one was rough. Here's a better example:}
+	◊img['src:"lilypond/first_example_good1.svg" 'class:"double"]
 	}
---
+++ lilypond/first_example_good1.ly
@@ -0,0 +1,61 @@
+\version "2.18.2"
+#(set-global-staff-size 18)
+\pointAndClickOff
+\paper{
+	between-system-space = 2.4\cm
+	indent=0\mm
+	line-width=120\mm
+	oddFooterMarkup=##f
+	oddHeaderMarkup=##f
+	bookTitleMarkup = ##f
+	scoreTitleMarkup = ##f
+}
+
+\layout {
+\context {
+    \Score
+    \remove "Bar_number_engraver"
+  }
+  \context {
+    \Staff
+    \override VerticalAxisGroup.default-staff-staff-spacing.basic-distance = #24
+  }
+  \context {
+  	\Voice 
+	\override Stem.length = #0
+	}
+}
+
+\header { 
+  tagline = "" 
+} 
+ 
+global= {
+  \time 4/4
+  \key c \major
+        \once \override Staff.TimeSignature #'stencil = ##f 
+}
+
+top = \new Voice \relative c'' {
+\override TextScript.outside-staff-padding = #1
+  \set Score.timing = ##f
+  \clef treble
+
+c1_"8"^"(1)" b_"6"^"(2)" d_"6"^"(3)" c_"6"^"(4)" c_"5"^"(5)" d_"5"^"(6)" c_"3"^"(7)" b_"3"^"(8)" a_"4"^"(9)" b_"6"^"(10)" c_"8"^"(11)"
+}
+
+cantus = \new Voice \relative c' {
+\override TextScript.outside-staff-padding = #1
+  \set Score.timing = ##f
+  \clef alto
+
+c1 d f e f g a g e d c
+}
+
+\score {
+\new StaffGroup <<
+\override StaffGroup.SystemStartBracket #'transparent = ##t
+	\new Staff << \global \top >>
+	\new Staff << \global \cantus >>
+	>>
+}
