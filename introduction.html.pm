22a23b4021dfd30cfd425f5d98fb8cbc600a1bf9
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2015-01-24 22:12:32 -0800

    Write the introduction

--- introduction.html.pm
+++ introduction.html.pm
@@ -3,3 +3,30 @@
 ◊header['id:"page-header"]{
 	◊page-title{Introduction}
 }
+◊main-section{◊p{This short, online book was written to serve as a very basic
+introduction to modal counterpoint for the beginner.}
+	◊p{That being said, I really have no business writing such a work. This
+	style of music was written by masters, and continues to be performed
+	centuries later. There is a wonderful selection of textbooks on this
+	subject both old and contemporary, written by some of the most
+	knowledgeable teachers and musicians in the world, that beautifully lay out the art of music
+	for anyone willing to dedicate the time and effort. (By
+	“contemporary”, I mean “not translated from the original latin.”) I, on the
+	other hand, am routinely outsmarted by the family dog, feeding him upwards
+	of three dinners a night because I trust his memory of the event more than
+	mine.}
+	◊p{So why read this book? Well, most books on the subject assume the
+	presence of an instructor. The reason for this is simple: this subject
+	requires the presence of an instructor. There are elements of this practice
+	that are quite subjective. Most subjective of all of them is the element of
+	beauty. You can no more learn how to write beautiful and compelling
+	counterpoint by reading a book than you can learn to take beautiful
+	photographs by reading a camera manual. This book attempts to briefly fill
+	in that gap for someone who might not have an instructor available. I have
+	tried to further explain some things that other books assume prior
+	knowledge of, and I offer poor examples along with more competent ones, so
+	that the difference may be examined. I hope you find it helpful.}
+	◊p{To navigate through the book, links to the previous and next chapters
+	may be found on the top and bottom of each page, as well as a link to the
+	table of contents.}}
+	
