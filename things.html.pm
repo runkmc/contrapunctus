f38613741d00ec4861b1cc8175bfd993112177e9
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2015-01-24 22:57:13 -0800

    Finish chapter

--- things.html.pm
+++ things.html.pm
@@ -2,3 +2,22 @@
 
 ◊header['id:"page-header"]{
 	◊page-title{Things I Didn’t Talk About}}
+
+◊main-section{◊p{The End!}
+	◊p{That was kind of abrupt. Sorry. You’ve reached the end, though. Good for
+	you!}
+	◊p{If you want to keep going, there are lots of wonderful books out there.
+	My three favorites are ◊a['href: "http://www.amazon.com/Modal-Counterpoint-Renaissance-Peter-Schubert/dp/019533194X/ref=sr_1_1?ie=UTF8&qid=1422167927&sr=8-1&keywords=modal+counterpoint"]{Modal Counterpoint}, 
+	◊a['href: "http://www.amazon.com/Counterpoint-Polyphonic-Vocal-Sixteenth-Century/dp/048627036X/ref=sr_1_1?ie=UTF8&qid=1422168094&sr=8-1&keywords=counterpoint+knud"]{Counterpoint: The Polyphonic Vocal Style of the Sixteenth Century}, and 
+	◊a['href:
+	"http://www.amazon.com/Study-Counterpoint-Johann-Joseph-Parnassum/dp/0393002772/ref=sr_1_1?ie=UTF8&qid=1422168190&sr=8-1&keywords=steps+to+parnassus"]{Study of Counterpoint}. 
+	There are other good books, but those are three of my favorites.}
+	◊p{The best thing you can do, however, is join a class. I stated at the
+	beginning of the book that an instructor is vital to truly understanding
+	this, and it really is true. Most four year colleges that offer degrees in
+	music should probably offer this course occasionally, and I wouldn’t be
+	surprised to find it at some community colleges with substantial music
+	departments as well. }
+	◊p{Otherwise, you can always contact me here with questions or with
+	exercises that you would like feedback on. Trust me, I like it.}
+	◊p{Thanks for reading!}}
